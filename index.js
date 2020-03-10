f77cd2e7f28d15fb4e184086385d1db548478347
SYED EBAD <syed.ebad1999@gmail.com>
2020-03-10 12:46:52 +0500

    make it batter

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const expressip = require('express-ip');
 const Users = require('./Users');
 
-const PORT = 5000 || process.env.PORT  ;
+const PORT = process.env.PORT || 5000 ;
 // const dbURL = 'mongodb://localhost/XORD' ;
 const dbURL = 'mongodb+srv://syedebad:ebad123@cluster0-xt32z.mongodb.net/XORD?retryWrites=true&w=majority';
 
